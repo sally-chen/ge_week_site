b86488025c1c807af680de98da920a48669790f6
Sally Chen <chuhan.chen@mail.utoronto.ca>
2017-03-05 17:41:41 +0000

    finalizing site

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,5 +23,8 @@ module GeWeek
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.initialize_on_precompile = false
+    config.consider_all_requests_local = true
   end
 end
